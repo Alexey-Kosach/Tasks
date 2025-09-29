ce1e6468a7461534b4bc1b21e153f6b06673fda9
Алексей Косач ТКИ-141 <alex_vk_007@mail.ru>
2025-09-29 14:07:23 +0300

    Add files via upload

---
+++ Task_2.2.c
@@ -0,0 +1,41 @@
+﻿#pragma warning(disable:4996)
+
+#define _USE_MATH_DEFINES
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * @brief - Расчет функции y по формуле
+ * @param x - значение переменной х
+ * @return - рассчитанное значение
+ */
+double getY(const double x, const double a);
+
+/**
+ * @brief - Точка входа в программу
+ * @return - вызвращает 0, если программа выполнена корректно
+ */
+int main()
+{
+    double x = 0;
+    double a = 1.1;
+    scanf("%lf", &x);
+    printf("y = %.2lf", getY(x, a));
+    return 0;
+}
+
+double getY(const double x, const double a)
+{
+    if (x > 1.2)
+    {
+        return log(x)*M_PI;
+    }
+    else if (x < 1.2)
+    {
+        return (cos(pow(x, 2))/ sin(pow(x, 2))) + (a*pow(x, 2)) / 2;
+    }
+    else
+    {
+        return 3;
+    }
+}
